38f9ab0967451d1fffe0ed8ebbf662d0a3f6e7ea
Igor-RnD <ibardyshev@gmail.com>
2025-12-23 13:38:48 +0300

    Update camera.py

--- devices/camera.py
+++ devices/camera.py
@@ -1,1 +1,8 @@
+from devices.base_device import BaseDevice
+
+class Camera(BaseDevice):
+    def stream(self):
+        if self.quarantined:
+            return "[BLOCKED]"
+        return "[VIDEO STREAM]"
 
