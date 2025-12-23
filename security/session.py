70999c4cf41707763fd3762063c03a0dc47121ac
Igor-RnD <ibardyshev@gmail.com>
2025-12-23 13:34:24 +0300

    Update session.py

--- security/session.py
+++ security/session.py
@@ -1,1 +1,22 @@
+import time
 
+class SessionStore:
+    def __init__(self):
+        self.sessions = {}
+
+    def create_session(self, user, role):
+        sid = f"{user}-{int(time.time())}"
+        self.sessions[sid] = {
+            "user": user,
+            "role": role,
+            "expires": time.time() + 300
+        }
+        return sid
+
+    def validate(self, sid):
+        session = self.sessions.get(sid)
+        if not session:
+            return None
+        if time.time() > session["expires"]:
+            return None
+        return session
