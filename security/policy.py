b25bd51e26d091cbac3bee2fe44b7551fe7348a9
Igor-RnD <ibardyshev@gmail.com>
2025-12-23 13:34:03 +0300

    Update policy.py

--- security/policy.py
+++ security/policy.py
@@ -1,1 +1,8 @@
+class PolicyStore:
+    def is_allowed(self, role, command):
+        if role == "ADMIN":
+            return True
+        if role == "USER" and command == "READ_CAMERA":
+            return True
+        return False
 
